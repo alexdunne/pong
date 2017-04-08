789f7aaaed739a6cd6df26d0f7f820463bc23c04
Alex Dunne <hi@alexdunne.net>
2017-04-08 02:39:41 +0100

    Reverted night sky background

--- client/src/index.js
+++ client/src/index.js
@@ -5,19 +5,8 @@ import { browserHistory } from 'react-router';
 import Routes from './routes';
 
 import './index.css';
-import './stars-background.css';
-
-const App = (props) => {
-  return (
-    <div>
-      <div className="stars"></div>
-      <div className="twinkling"></div>
-      <Routes history={props.browserHistory} />
-    </div>
-  );
-};
 
 ReactDOM.render(
-  <App history={browserHistory} />,
+  <Routes history={browserHistory} />,
   document.getElementById('root')
 );
