2407abe9b718e82c3fab622a9cee06bfacf5f32c
Alex Dunne <hi@alexdunne.net>
2017-04-08 03:19:12 +0100

    Added new + join game to main menu with retro style font

--- client/src/pages/MainMenu/index.js
+++ client/src/pages/MainMenu/index.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import './index.css';
 
 class MainMenu extends Component {
   state = {  }
   render() {
     return (
-      <span>Main menu</span>
+      <div className="main-menu fill-parent">
+        <div className="main-menu__content">  
+          <ul className="main-menu__links">
+            <li className="main-menu__link"><Link to="/new">New Game</Link></li>
+            <li className="main-menu__link"><Link to="/join">Join Game</Link></li>
+          </ul>
+        </div>
+      </div>
     );
   }
 }
